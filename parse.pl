7301cf55107be2a88630d47cb299fa907d35cf61
Damien <damien@dam.io>
2015-03-09 15:03:30 +0100

    le boudin et la choucroute et du chocolat si t'as des bras

--- parse.pl
+++ parse.pl
@@ -3,18 +3,12 @@
 use strict;
 use warnings;
 
-#print "$_ $h{$_}\n" for (keys %h);
-
-
-#######################################
-######### Hash to XML for each file
-
 my $output_filepath = "global.xml";
 my $bulletins_dir = "BULLETINS/";
 my @files = `ls $bulletins_dir`;
 open(my $fh, '>', $output_filepath);
 print $fh '<?xml version="1.0" encoding="UTF-8" ?>\n';
-print $fh '<corpus>\n';
+print $fh "<corpus>\n";
 foreach my $filename (@files) {
 	chomp $filename;
 
@@ -42,17 +36,22 @@ foreach my $filename (@files) {
 	$html =~ /<span class="style17">(.*)<\/span/;
 	$h{title} = $1;
 
-	$html =~ /<p class="style44"><span class="style85">(.*)- email : /;
-	$h{contact_name} = $1;
+	$h{contact_name} = "";
+	if($html =~ /<p class="style44"><span class="style85">(.*)- email : /){
+		$h{contact_name} = $1;
+	}
 
-	$html =~ /email : <a href="mailto:(.*)">/;
-	$h{contact_mail} = $1;
+	$h{contact_mail} = "";
+	if($html =~ /email : <a href="mailto:(.*)">/){
+		$h{contact_mail} = $1;
+	}
 
 	$html =~ s/\n//g;
 	$html =~ /<p class="style96"><span class="style95">(.*)<\/span><\/p><\/td>.*<td width=133 valign=top bgcolor="#dae1e8" class="FWExtra2">/;
 	$h{content} = $1;
 
 	my @imgs = $h{content} =~ /<img src="([^<]*)" border/g;
+	my @legends = $h{content} =~ /<img src="([^<]*)" border/g;
 
 	#clean the content
 	while($h{content} =~ /<\/span>.*<span class="style95">/){
@@ -70,7 +69,7 @@ foreach my $filename (@files) {
 	foreach (@imgs){
 		print $fh "<img><urlImage>$_</urlImage><legendeImage>TODO</legendeImage></img>\n";
 	}
-	print $fh "<contact>$h{contact_name} - $h{contact_mail}</contact>\n";
+	print $fh "<contact>$h{contact_name} $h{contact_mail}</contact>\n";
 	print $fh "</bulletin>\n";
 }
 print $fh '</corpus>\n';
