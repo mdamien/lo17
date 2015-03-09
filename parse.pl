ca666d4181b59841907d314f5f51be8cdb576ee2
Damien <damien@dam.io>
2015-03-09 16:00:29 +0100

    snif, la vie c'est comme la vache qui rie!

--- parse.pl
+++ parse.pl
@@ -42,7 +42,7 @@ foreach my $filename (@files) {
 	}
 
 	$h{contact_mail} = "";
-	if($html =~ /email : <a href="mailto:(.*)">/){
+	if($html =~ /email : <a href="mailto:(.*)"\/>/){
 		$h{contact_mail} = $1;
 	}
 
@@ -50,8 +50,17 @@ foreach my $filename (@files) {
 	$html =~ /<p class="style96"><span class="style95">(.*)<\/span><\/p><\/td>.*<td width=133 valign=top bgcolor="#dae1e8" class="FWExtra2">/;
 	$h{content} = $1;
 
-	my @imgs = $h{content} =~ /<img src="([^<]*)" border/g;
-	my @legends = $h{content} =~ /<img src="([^<]*)" border/g;
+	my @imgs = $h{content} =~ /<img src="([^<"]*)"[^>]*><br \/><span class="style\d\d">(<strong>|)([^<]*)/g; #88/21/95
+	#print "lol\n";
+	foreach(@imgs){
+		#print "LOOOOL".$_."\n";
+	}
+	#print "mdr\n";
+
+
+	#TODO REGEX OK => Catch multiple ? DO I have to divide the array by 3 ???
+
+	#print @imgs[0];
 
 	#clean the content
 	while($h{content} =~ /<\/span>.*<span class="style95">/){
@@ -66,11 +75,17 @@ foreach my $filename (@files) {
 	print $fh "<rubrique>$h{rubrique}</rubrique>\n";
 	print $fh "<titre>$h{title}</titre>\n";
 	print $fh "<texte>$h{content}</texte>\n";
-	foreach (@imgs){
-		print $fh "<img><urlImage>$_</urlImage><legendeImage>TODO</legendeImage></img>\n";
+	print $fh "<images>\n";
+	foreach my $lol (@imgs){
+		#foreach(@img){print $_."\n";}
+		#print $fh "<image><urlImage>".$lol."</urlImage><legendeImage>"."todo"."</legendeImage></img>\n";
 	}
-	print $fh "<contact>$h{contact_name} $h{contact_mail}</contact>\n";
+	print $fh "</images>\n";
+	print $fh "<contact>TODO SIMPLE</contact>\n";
 	print $fh "</bulletin>\n";
 }
 print $fh "</corpus>\n";
 close $fh;
+
+
+
