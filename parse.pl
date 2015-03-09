63a3feb7e852aabd5c283fba3f232c4d3ff29cec
Damien <damien@dam.io>
2015-03-09 14:49:58 +0100

    first XML with almost everything inside (but invalid XML)

--- parse.pl
+++ parse.pl
@@ -3,53 +3,75 @@
 use strict;
 use warnings;
 
-my %h = ();
+#print "$_ $h{$_}\n" for (keys %h);
 
-my $html = do { local $/; <> };
 
-$html =~ /(\d\d\d\d\/\d\d\/\d\d)/;
-$h{date} = $1;
+#######################################
+######### Hash to XML for each file
 
-$html =~ /BE_France_(\d+)/;
-$h{bn_number} = $1;
+my $output_filepath = "global.xml";
+my $bulletins_dir = "BULLETINS/";
+my @files = `ls $bulletins_dir`;
+open(my $fh, '>', $output_filepath);
+print $fh '<?xml version="1.0" encoding="UTF-8" ?>\n';
+print $fh '<corpus>\n';
+foreach my $filename (@files) {
+	chomp $filename;
 
-$html =~ /<p class="style96"><span class="style42">(.*)<br></;
-$h{rubrique} = $1;
+	my $fullpath = $bulletins_dir.$filename;
 
-$html =~ /<span class="style17">(.*)<\/span/;
-$h{title} = $1;
+	my %h = ();
 
-$html =~ /<p class="style44"><span class="style85">(.*)- email : /;
-$h{contact_name} = $1;
+    my $html;
+    open(my $fh2, '<', $fullpath) or die "cannot open file $fullpath";
+    {
+        local $/;
+        $html = <$fh2>;
+    }
+    close($fh2);
 
-$html =~ /email : <a href="mailto:(.*)">/;
-$h{contact_mail} = $1;
+	$html =~ /(\d\d\d\d\/\d\d\/\d\d)/;
+	$h{date} = $1;
 
-$html =~ s/\n//g;
-$html =~ /<p class="style96"><span class="style95">(.*)<\/span><\/p><\/td>.*<td width=133 valign=top bgcolor="#dae1e8" class="FWExtra2">/;
-$h{content} = $1;
+	$html =~ /BE_France_(\d+)/;
+	$h{bn_number} = $1;
 
-my @imgs = $h{content} =~ /<img src="([^<]*)" border/g;
-$h{imgs} = join(";",@imgs);
+	$html =~ /<p class="style96"><span class="style42">(.*)<br></;
+	$h{rubrique} = $1;
 
-#clean the content
-while($h{content} =~ /<\/span>.*<span class="style95">/){
-	$h{content} =~ /^(.*)<\/span>.*<span class="style95">(.*)$/;
-	$h{content} = $1.$2;
+	$html =~ /<span class="style17">(.*)<\/span/;
+	$h{title} = $1;
+
+	$html =~ /<p class="style44"><span class="style85">(.*)- email : /;
+	$h{contact_name} = $1;
+
+	$html =~ /email : <a href="mailto:(.*)">/;
+	$h{contact_mail} = $1;
+
+	$html =~ s/\n//g;
+	$html =~ /<p class="style96"><span class="style95">(.*)<\/span><\/p><\/td>.*<td width=133 valign=top bgcolor="#dae1e8" class="FWExtra2">/;
+	$h{content} = $1;
+
+	my @imgs = $h{content} =~ /<img src="([^<]*)" border/g;
+
+	#clean the content
+	while($h{content} =~ /<\/span>.*<span class="style95">/){
+		$h{content} =~ /^(.*)<\/span>.*<span class="style95">(.*)$/;
+		$h{content} = $1.$2;
+	}
+
+	print $fh "<bulletin>\n";
+	print $fh "<fichier>$filename</fichier>\n";
+	print $fh "<numero>$h{bn_number}</numero>\n";
+	print $fh "<date>$h{date}</date>\n";
+	print $fh "<rubrique>$h{rubrique}</rubrique>\n";
+	print $fh "<titre>$h{title}</titre>\n";
+	print $fh "<texte>$h{content}</texte>\n";
+	foreach (@imgs){
+		print $fh "<img><urlImage>$_</urlImage><legendeImage>TODO</legendeImage></img>\n";
+	}
+	print $fh "<contact>$h{contact_name} - $h{contact_mail}</contact>\n";
+	print $fh "</bulletin>\n";
 }
-
-print "\n";
-
-print "$_ $h{$_}\n" for (keys %h);
-
-#print "\n";
-#$h{content} =~ /<img/;
-#print $1;
-
-#for f in BULLETINS/*;do ./td1.pl < $f;done
-
-
-# xml = "<dssd>"
-# for fichier in fichier:
-# 	xml += parse(sdsd)
-# xml = "</sdsd>"
+print $fh '</corpus>\n';
+close $fh;
