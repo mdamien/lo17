b4e65acd011d828bd7e4df34d1821b591cd27479
Damien <damien@dam.io>
2015-03-16 14:53:13 +0100

    correct tfidf and 80% selection

--- tf.py
+++ tf.py
@@ -2,7 +2,6 @@ import math
 
 tfs = {}
 df = {}
-N = 0
 for line in open('mots.tsv'):
 	word, f, n, rubrique = line.split('\t')
 	if f not in tfs:
@@ -15,8 +14,8 @@ for line in open('mots.tsv'):
 			df[word] = 0
 		df[word] += 1
 	freqs[word] += 1
-	N += 1
 
+N = len(tfs.keys())
 idf = {w:math.log(N/df[w])for w in df}
 
 results = []
@@ -26,6 +25,7 @@ for f, freqs in tfs.items():
 		t = count/sum_f
 		results.append([f,w,t,idf[w],t*idf[w]])
 
-results = [['file','word','tf','idf','tfidf']]+sorted(results, key=lambda x:x[-1])
+results = [['file','word','tf','idf','tfidf']]+sorted(results, key=lambda x:-x[-1]) #sort by tfidf
+results = results[:int(len(results)*0.8)] #take the first 80%
 for r in results:
 	print('\t'.join(str(x) for x in r))
