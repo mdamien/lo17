b52da88b1df7748f524f81a69dc91c99b1bbf7a0
Damien <damien@dam.io>
2015-03-16 15:32:58 +0100

    stats

---
+++ tab_words.py
@@ -0,0 +1,19 @@
+tfidfs = {}
+for line in open('out/tfidf.tsv'):
+	f, w, tf, idf, tfidf = line.split('\t')
+	if w not in tfidfs:
+		tfidfs[w] = []
+	tfidfs[w].append(float(tfidf))
+
+def stat(func,name):
+	tab = []
+	for word, scores in tfidfs.items():
+		tab.append([word, func(scores)])
+	tab.sort(key=lambda x:-x[1])
+	with open('out/stats/%s' % name,'w') as f:
+		for line in tab:
+			f.write('%s %d\n' % (line[0], int(line[1]*1000)))
+
+stat(lambda x:max(x),'max')
+stat(lambda x:min(x),'min')
+stat(lambda x:sum(x)/len(x),'mean')
