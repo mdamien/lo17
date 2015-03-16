47e1732c1b12d4e6a9f9ebb05c25e717677502e7
maximool <maxime.margerin@gmail.com>
2015-03-16 12:50:29 +0100

    Rajouté les conversions pour les autres éléments du hash. Global.xml bien formé.

--- README.md
+++ README.md
@@ -5,11 +5,10 @@ Awesome project. Such wow. Many pizzas.
 ![yeah](http://i.giphy.com/wWdA3Z9n3qWMU.gif)
 
 ## TODO list
-[x] structuration du projet
-[x] fusion des scripts
-[ ] fin du script parse.pl
-[ ] interface global.xml / base de données
-[ ] schéma de validation du fichier global.xml, script ANT à coder (pour remplir quelle tâche ? Regarder le paragraphe des suggestion)
+- [x] structuration du projet
+- [x] fusion des scripts
+- [x] fin du script parse.pl
+- [ ] interface global.xml / base de données
 
 ## Structure du projet
 Lancer le script parse.pl qui va parser chaque élément de l'archive et en extraire les informations pertinentes. Le script main va ensuite insérer ces informations dans un gros fichier (global.xml). Ce fichier global.xml va initaliser une base de données relationnelle (PostgreSQL), dont développer en Java une sorte de mini moteur de recherche capable de comprendre.
--- parse.pl
+++ parse.pl
@@ -60,30 +60,130 @@ foreach my $filename (@files) {
 
 	#special characters
 	$h{content} =~ s/&nbsp;/ /g;
-    $h{content} =~ s/&ecirc;/ê/g;
-    $h{content} =~ s/&ouml;/ö/g;
-    $h{content} =~ s/&ucirc;/û/g;
-    $h{content} =~ s/&egrave;/è/g;
-    $h{content} =~ s/&icirc;/î/g;
-    $h{content} =~ s/&eacute;/é/g;
-    $h{content} =~ s/&quot;/'/g;
-    $h{content} =~ s/&agrave;/à/g;
-    $h{content} =~ s/&acirc;/â/g;
-    $h{content} =~ s/&iuml;/ï/g;
-    $h{content} =~ s/&ocirc;/ô/g;
-    $h{content} =~ s/&ccedil;/ç/g;
-    $h{content} =~ s/&euml;/ë/g;
-    $h{content} =~ s/&ugrave;/ù/g;
-    $h{content} =~ s/&szlig;/ß/g;
-    $h{content} =~ s/&uuml;/ü/g;
-    $h{content} =~ s/&amp;/et/g;
-    $h{content} =~ s/&deg;/°/g;
-    $h{content} =~ s/&ograve;/ò/g;
-    $h{content} =~ s/&ecirc;/ê/g;
-    $h{content} =~ s/&#338;/Œ/g;
-    $h{content} =~ s/&#8211/—/g;
-    $h{content} =~ s/&#21;/--/g;
+	$h{content} =~ s/&ecirc;/ê/g;
+	$h{content} =~ s/&ouml;/ö/g;
+	$h{content} =~ s/&ucirc;/û/g;
+	$h{content} =~ s/&egrave;/è/g;
+	$h{content} =~ s/&icirc;/î/g;
+	$h{content} =~ s/&eacute;/é/g;
+	$h{content} =~ s/&quot;/'/g;
+	$h{content} =~ s/&agrave;/à/g;
+	$h{content} =~ s/&acirc;/â/g;
+	$h{content} =~ s/&iuml;/ï/g;
+	$h{content} =~ s/&ocirc;/ô/g;
+	$h{content} =~ s/&ccedil;/ç/g;
+	$h{content} =~ s/&euml;/ë/g;
+	$h{content} =~ s/&ugrave;/ù/g;
+	$h{content} =~ s/&szlig;/ß/g;
+	$h{content} =~ s/&uuml;/ü/g;
+	$h{content} =~ s/&amp;/et/g;
+	$h{content} =~ s/&deg;/°/g;
+	$h{content} =~ s/&ograve;/ò/g;
+	$h{content} =~ s/&ecirc;/ê/g;
+	$h{content} =~ s/&#338;/Œ/g;
+	$h{content} =~ s/&#8211/—/g;
+	$h{content} =~ s/&#21;/--/g;
 
+	$h{rubrique} =~ s/&nbsp;/ /g;
+	$h{rubrique} =~ s/&ecirc;/ê/g;
+	$h{rubrique} =~ s/&ouml;/ö/g;
+	$h{rubrique} =~ s/&ucirc;/û/g;
+	$h{rubrique} =~ s/&egrave;/è/g;
+	$h{rubrique} =~ s/&icirc;/î/g;
+	$h{rubrique} =~ s/&eacute;/é/g;
+	$h{rubrique} =~ s/&quot;/'/g;
+	$h{rubrique} =~ s/&agrave;/à/g;
+	$h{rubrique} =~ s/&acirc;/â/g;
+	$h{rubrique} =~ s/&iuml;/ï/g;
+	$h{rubrique} =~ s/&ocirc;/ô/g;
+	$h{rubrique} =~ s/&ccedil;/ç/g;
+	$h{rubrique} =~ s/&euml;/ë/g;
+	$h{rubrique} =~ s/&ugrave;/ù/g;
+	$h{rubrique} =~ s/&szlig;/ß/g;
+	$h{rubrique} =~ s/&uuml;/ü/g;
+	$h{rubrique} =~ s/&amp;/et/g;
+	$h{rubrique} =~ s/&deg;/°/g;
+	$h{rubrique} =~ s/&ograve;/ò/g;
+	$h{rubrique} =~ s/&ecirc;/ê/g;
+	$h{rubrique} =~ s/&#338;/Œ/g;
+	$h{rubrique} =~ s/&#8211/—/g;
+	$h{rubrique} =~ s/&#21;/--/g;
+
+	$h{title} =~ s/&nbsp;/ /g;
+	$h{title} =~ s/&ecirc;/ê/g;
+	$h{title} =~ s/&ouml;/ö/g;
+	$h{title} =~ s/&ucirc;/û/g;
+	$h{title} =~ s/&egrave;/è/g;
+	$h{title} =~ s/&icirc;/î/g;
+	$h{title} =~ s/&eacute;/é/g;
+	$h{title} =~ s/&quot;/'/g;
+	$h{title} =~ s/&agrave;/à/g;
+	$h{title} =~ s/&acirc;/â/g;
+	$h{title} =~ s/&iuml;/ï/g;
+	$h{title} =~ s/&ocirc;/ô/g;
+	$h{title} =~ s/&ccedil;/ç/g;
+	$h{title} =~ s/&euml;/ë/g;
+	$h{title} =~ s/&ugrave;/ù/g;
+	$h{title} =~ s/&szlig;/ß/g;
+	$h{title} =~ s/&uuml;/ü/g;
+	$h{title} =~ s/&amp;/et/g;
+	$h{title} =~ s/&deg;/°/g;
+	$h{title} =~ s/&ograve;/ò/g;
+	$h{title} =~ s/&ecirc;/ê/g;
+	$h{title} =~ s/&#338;/Œ/g;
+	$h{title} =~ s/&#8211/—/g;
+	$h{title} =~ s/&#21;/--/g;
+
+	$h{contact_name} =~ s/&nbsp;/ /g;
+	$h{contact_name} =~ s/&ecirc;/ê/g;
+	$h{contact_name} =~ s/&ouml;/ö/g;
+	$h{contact_name} =~ s/&ucirc;/û/g;
+	$h{contact_name} =~ s/&egrave;/è/g;
+	$h{contact_name} =~ s/&icirc;/î/g;
+	$h{contact_name} =~ s/&eacute;/é/g;
+	$h{contact_name} =~ s/&quot;/'/g;
+	$h{contact_name} =~ s/&agrave;/à/g;
+	$h{contact_name} =~ s/&acirc;/â/g;
+	$h{contact_name} =~ s/&iuml;/ï/g;
+	$h{contact_name} =~ s/&ocirc;/ô/g;
+	$h{contact_name} =~ s/&ccedil;/ç/g;
+	$h{contact_name} =~ s/&euml;/ë/g;
+	$h{contact_name} =~ s/&ugrave;/ù/g;
+	$h{contact_name} =~ s/&szlig;/ß/g;
+	$h{contact_name} =~ s/&uuml;/ü/g;
+	$h{contact_name} =~ s/&amp;/et/g;
+	$h{contact_name} =~ s/&deg;/°/g;
+	$h{contact_name} =~ s/&ograve;/ò/g;
+	$h{contact_name} =~ s/&ecirc;/ê/g;
+	$h{contact_name} =~ s/&#338;/Œ/g;
+	$h{contact_name} =~ s/&#8211/—/g;
+	$h{contact_name} =~ s/&#21;/--/g;
+
+	$h{contact_mail} =~ s/&nbsp;/ /g;
+	$h{contact_mail} =~ s/&ecirc;/ê/g;
+	$h{contact_mail} =~ s/&ouml;/ö/g;
+	$h{contact_mail} =~ s/&ucirc;/û/g;
+	$h{contact_mail} =~ s/&egrave;/è/g;
+	$h{contact_mail} =~ s/&icirc;/î/g;
+	$h{contact_mail} =~ s/&eacute;/é/g;
+	$h{contact_mail} =~ s/&quot;/'/g;
+	$h{contact_mail} =~ s/&agrave;/à/g;
+	$h{contact_mail} =~ s/&acirc;/â/g;
+	$h{contact_mail} =~ s/&iuml;/ï/g;
+	$h{contact_mail} =~ s/&ocirc;/ô/g;
+	$h{contact_mail} =~ s/&ccedil;/ç/g;
+	$h{contact_mail} =~ s/&euml;/ë/g;
+	$h{contact_mail} =~ s/&ugrave;/ù/g;
+	$h{contact_mail} =~ s/&szlig;/ß/g;
+	$h{contact_mail} =~ s/&uuml;/ü/g;
+	$h{contact_mail} =~ s/&amp;/et/g;
+	$h{contact_mail} =~ s/&deg;/°/g;
+	$h{contact_mail} =~ s/&ograve;/ò/g;
+	$h{contact_mail} =~ s/&ecirc;/ê/g;
+	$h{contact_mail} =~ s/&#338;/Œ/g;
+	$h{contact_mail} =~ s/&#8211/—/g;
+	$h{contact_mail} =~ s/&#21;/--/g;
+	
 	print $fh "<bulletin>\n";
 	print $fh "<fichier>$filename</fichier>\n";
 	print $fh "<numero>$h{bn_number}</numero>\n";
@@ -93,7 +193,31 @@ foreach my $filename (@files) {
 	print $fh "<texte>$h{content}</texte>\n";
 	print $fh "<images>\n";
 	for(my $i = 0; $i+2 < scalar(@imgs); $i += 3){
-		print $fh "<image><urlImage>".$imgs[$i]."</urlImage><legendeImage>".$imgs[$i+2]."</legendeImage></img>\n";
+		$imgs[$i+2] =~ s/&nbsp;/ /g;
+		$imgs[$i+2] =~ s/&ecirc;/ê/g;
+		$imgs[$i+2] =~ s/&ouml;/ö/g;
+		$imgs[$i+2] =~ s/&ucirc;/û/g;
+		$imgs[$i+2] =~ s/&egrave;/è/g;
+		$imgs[$i+2] =~ s/&icirc;/î/g;
+		$imgs[$i+2] =~ s/&eacute;/é/g;
+		$imgs[$i+2] =~ s/&quot;/'/g;
+		$imgs[$i+2] =~ s/&agrave;/à/g;
+		$imgs[$i+2] =~ s/&acirc;/â/g;
+		$imgs[$i+2] =~ s/&iuml;/ï/g;
+		$imgs[$i+2] =~ s/&ocirc;/ô/g;
+		$imgs[$i+2] =~ s/&ccedil;/ç/g;
+		$imgs[$i+2] =~ s/&euml;/ë/g;
+		$imgs[$i+2] =~ s/&ugrave;/ù/g;
+		$imgs[$i+2] =~ s/&szlig;/ß/g;
+		$imgs[$i+2] =~ s/&uuml;/ü/g;
+		$imgs[$i+2] =~ s/&amp;/et/g;
+		$imgs[$i+2] =~ s/&deg;/°/g;
+		$imgs[$i+2] =~ s/&ograve;/ò/g;
+		$imgs[$i+2] =~ s/&ecirc;/ê/g;
+		$imgs[$i+2] =~ s/&#338;/Œ/g;
+		$imgs[$i+2] =~ s/&#8211/—/g;
+		$imgs[$i+2] =~ s/&#21;/--/g;		
+		print $fh "<image><urlImage>".$imgs[$i]."</urlImage><legendeImage>".$imgs[$i+2]."</legendeImage></image>\n";
 	}
 	print $fh "</images>\n";
 	print $fh "<contact><a href=\"$h{contact_mail}\">$h{contact_name}</a></contact>\n";
@@ -102,11 +226,7 @@ foreach my $filename (@files) {
 print $fh "</corpus>\n";
 close $fh;
 
-
-
-
 #ls -l BULLETINS/ | wc -l
 # -> 327 -> 326 files
 #cat global.xml | grep "<bulletin>" | wc -l
 # -> 326
-
