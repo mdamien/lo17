0f95e69fc01c3901361886cc9914eb47dbbc1d20
maximool <maxime.margerin@gmail.com>
2015-03-23 15:17:30 +0100

    Message failed

--- discri.pl
+++ discri.pl
@@ -1,5 +1,7 @@
+# Ce fichier dresse la liste des mots à retirer, les enregistre dans le fichier newcreeFiltreArgument. Ce fichier va ensuite
+
 use strict;
-use warnings;
+#use warnings;
 
 my $i = 0;
 my @tab;
@@ -7,12 +9,14 @@ open (FH, ">", "newcreeFiltreArgument") or die "fuck";
 open (FHA, "<", "$ARGV[0]") or die "fuck";
 while (<FHA>) {
 	my @ligne = split (";", $_);
-	if ($i < 14400) {
-		if(length($ligne[0]) > 2) {
-			print length($ligne[0]) . "\n";
-			print FH $ligne[0] . "\n";
-		}
+	# Met dans le fichier tous les mots dont le score est le plus faible (premier quartile de tfidf)
+	if ($i > 14500) {
+		print FH $ligne[0] . "\n";
 	}
+	# Pour les autres mots, on retire les chiffres simples et les mots dont la longueur est 1 ou 2
+#	elsif (length($ligne[0] <= 2)) {
+#		print FH $ligne[0] . "\n";
+#	}
 	$i++;
 }
 close FHA;
