7e1d3098e4ad3167040c412a3ab6b406e52d8f98
Damien <damien@dam.io>
2015-03-23 13:43:24 +0100

    add percentiles

--- tab_words.py
+++ tab_words.py
@@ -1,3 +1,5 @@
+import functools, math
+
 tfidfs = {}
 for line in open('out/tfidf.tsv'):
 	f, w, tf, idf, tfidf = line.split('\t')
@@ -5,26 +7,33 @@ for line in open('out/tfidf.tsv'):
 		tfidfs[w] = []
 	tfidfs[w].append(float(tfidf))
 
-def stat(func,name):
+def stat(func, name):
 	tab = []
 	for word, scores in tfidfs.items():
 		tab.append([word, func(scores)])
 	tab.sort(key=lambda x:-x[1])
 	with open('out/stats/%s' % name,'w') as f:
 		for line in tab:
-			f.write('%s %d\n' % (line[0], int(line[1]*1000)))
+			f.write('{:>30}       {:<30}\n'.format(line[0], line[1]*1000))
 
 stat(lambda x:max(x),'max')
 stat(lambda x:min(x),'min')
 stat(lambda x:sum(x)/len(x),'mean')
 
-def median(lst):
-    lst = sorted(lst)
-    if len(lst) < 1:
-            return None
-    if len(lst) %2 == 1:
-            return lst[((len(lst)+1)//2)-1]
-    if len(lst) %2 == 0:
-            return float(sum(lst[(len(lst)//2)-1:(len(lst)//2)+1]))/2
+def percentile(N, percent):
+    N.sort()
+    if not N:
+        return None
+    k = (len(N)-1) * percent
+    f = math.floor(k)
+    c = math.ceil(k)
+    if f == c:
+        return N[int(k)]
+    d0 = N[int(f)] * (c-k)
+    d1 = N[int(c)] * (k-f)
+    return d0+d1
+
+median = functools.partial(percentile, percent=0.5)
 
 stat(median,'median')
+stat(lambda x: percentile(x,0.25), 'first_quartile')
