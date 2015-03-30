e70d54996a4f0c971a36718f7792b16ccabfda09
maximool <maxime.margerin@gmail.com>
2015-03-30 15:42:45 +0200

    mESSAGE fAILED

--- lo17td05/src/td/Lexique.java
+++ lo17td05/src/td/Lexique.java
@@ -5,6 +5,7 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Hashtable;
+import java.util.Iterator;
 import java.util.Set;
 import java.util.StringTokenizer;
 import java.util.TreeSet;
@@ -41,8 +42,8 @@ public class Lexique {
 		if(words.containsKey(chaine)){
 			lemmes.add(words.get(chaine));
 		}
-		//TODO test nb lettres communes
 		else{
+			//TODO Levenshtein
 			ArrayList<Match> matches = Levenshtein.best_matches(chaine, words.keySet(), 1);
 			for (Match match : matches) {
 				if(!lemmes.contains(match.word)){
@@ -52,8 +53,26 @@ public class Lexique {
 					}
 				}
 			}
+			//TODO Lettres communes
+			Hashtable<String, Integer> commonLettersHash = new Hashtable<String, Integer>();
+			Iterator<String> jtr = words.values().iterator();
+			while(jtr.hasNext()){
+				String curr = jtr.next();
+				commonLettersHash.put(curr, lettersInCommon(chaine, curr));
+			}
 		}
 		return lemmes;
 	}
 
+	public int lettersInCommon(String a, String b) {
+		int charsInCommon = -1;
+		char[] aChars = new char[a.length()];
+		a.getChars(0, a.length() - 1, aChars, 0);
+		for (int i = 0; i < aChars.length; i++) {
+			if (b.contains(""+aChars[i])) {
+				charsInCommon++;
+			}
+		}
+		return charsInCommon;
+	}
 }
