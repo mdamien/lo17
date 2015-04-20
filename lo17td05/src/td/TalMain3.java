fa07ba1e2e8b87408d6a0ea26ec88d06b2a00071
maximool <maxime.margerin@gmail.com>
2015-04-20 13:20:57 +0200

    ANTLRWORKS integration 3

--- lo17td05/src/td/TalMain3.java
+++ lo17td05/src/td/TalMain3.java
@@ -7,7 +7,6 @@ import org.antlr.runtime.ANTLRReaderStream;
 import org.antlr.runtime.CommonTokenStream;
 
 public class TalMain3 {
-
 	public static void main(String args[]) {
 		Scanner scanner = new Scanner(System.in);
 		System.out.print("Texte : ");
